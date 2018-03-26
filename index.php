3b61a919cf46c4af6e4467c8b15def094cb039c9
Cheng-En Wu <laker1234562001@gmail.com>
2018-03-26 22:54:59 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+      $dbhost = 'mysql';
+      $dbuser = 'root';
+      $dbpass = 'password';
+      $dbname = 'test';
+      $conn = new mysqli($dbhost, $dbuser, $dbpass,$dbname);
+      $sql = "select * from `message`";
+      $result = mysqli_query($conn,$sql);
+      while ($row = mysqli_fetch_array($result)){
+        $Array_Return =array(
+          "message" => $row['message']
+        );
+      }
+      echo json_encode($Array_Return);
+?>
