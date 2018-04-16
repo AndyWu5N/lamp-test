a4356fdea9e4e27728735d797bcd941217143337
Cheng-En Wu <laker1234562001@gmail.com>
2018-04-16 21:09:55 +0800

    update index.php

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
 <?php
-      $dbhost = 'mysql';
+      $dbhost = 'mysql-0.mysql';
       $dbuser = 'root';
-      $dbpass = 'password';
+      $dbpass = '';
       $dbname = 'test';
-      $conn = new mysqli($dbhost, $dbuser, $dbpass,$dbname);
-      $sql = "select * from `messages`";
-      $result = mysqli_query($conn,$sql);
-      while ($row = mysqli_fetch_array($result)){
-        $Array_Return =array(
+      $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+      $sql = "INSERT INTO `messages` (`message`) VALUES (`hello`);";
+      $result = mysqli_query($conn, $sql);
+      while ($row = mysqli_fetch_array($result)) {
+        $Array_Return = array(
           "message" => $row['message']
         );
       }
       echo json_encode($Array_Return);
+      echo "\n";
       echo getHostByName(getHostName());
+      echo "\n";
+
+      $dbhost = 'mysql-read';
+      $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+      $sql = "select * from `messages`";
+      $result = mysqli_query($conn, $sql);
 ?>
