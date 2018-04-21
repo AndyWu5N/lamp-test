ba5ffae6e3518c55893665120c202f9d638099aa
Cheng-En Wu <laker1234562001@gmail.com>
2018-04-21 15:28:53 +0800

    add Dockerfile modify index.php

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php
       $dbhost = 'mysql-0.mysql';
       $dbuser = 'root';
-      $dbpass = '';
+      $dbpass = 'password';
       $dbname = 'test';
       $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-      $sql = "INSERT INTO `messages` (`message`) VALUES (`hello`);";
+      $sql = "INSERT INTO messages (message) VALUES ('Hello2')";
       $result = mysqli_query($conn, $sql);
-      while ($row = mysqli_fetch_array($result)) {
-        $Array_Return = array(
-          "message" => $row['message']
-        );
-      }
-      echo json_encode($Array_Return);
+
+      echo $result;
       echo "\n";
       echo getHostByName(getHostName());
       echo "\n";
@@ -20,4 +16,10 @@
       $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
       $sql = "select * from `messages`";
       $result = mysqli_query($conn, $sql);
+      while ($row = mysqli_fetch_array($result)) {
+        $Array_Return = array(
+          "message" => $row['message']
+        );
+      }
+      echo json_encode($Array_Return);
 ?>
